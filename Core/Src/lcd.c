222b15067413df1753b46db3e8342822cf9bf305
marchel99 <kubamarchelski@live.com>
2023-12-08 16:34:41 +0100

    Pierwszy commit

---
+++ Core/Src/lcd.c
@@ -0,0 +1,11 @@
+/*
+ * lcd.c
+ *
+ *  Created on: Dec 8, 2023
+ *      Author: mrchl
+ */
+
+
+
+#include "lcd.h"
+#include "spi.h"
---
+++ Core/Src/lcd.h
@@ -0,0 +1,12 @@
+/*
+ * lcd.c
+ *
+ *  Created on: Dec 8, 2023
+ *      Author: mrchl
+ */
+
+#pragma once
+
+#include <stdint.h>
+
+void lcd_init(void);
